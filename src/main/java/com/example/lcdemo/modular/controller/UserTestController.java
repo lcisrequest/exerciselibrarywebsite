f1ac8ab69dfc9c8130dcc69b66d3f81d9340630f
李晨lc <1026008745@qq.com>
2018-03-02 17:15:35 +0800

    添加顺序练习和随机练习提交，模拟练习提交

--- src/main/java/com/example/lcdemo/modular/controller/UserTestController.java
+++ src/main/java/com/example/lcdemo/modular/controller/UserTestController.java
@@ -2,9 +2,11 @@ package com.example.lcdemo.modular.controller;
 
 import com.example.lcdemo.base.controller.BaseController;
 import com.example.lcdemo.base.tips.SuccessTip;
+import com.example.lcdemo.modular.admin.dto.UserTestDTO;
 import com.example.lcdemo.modular.admin.service.UserTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +21,34 @@ public class UserTestController extends BaseController {
 
     /**
      * 生成一套指定类型的顺序练习
-     * @param testNum
-     * @param testType
      * @return
      */
     @RequestMapping("/getOrderTest")
-    public ResponseEntity getAOrderTest(int testNum,String testType){
-        List<Map<String,Object>> list = userTestService.getAOrderTest(getUserId(),testNum,testType);
+    public ResponseEntity getAOrderTest(int testNum,String courseType){
+        List<Map<String,Object>> list = userTestService.getAOrderTest(getUserId(),testNum,courseType);
+        return ResponseEntity.ok(SuccessTip.create(list,"请求成功"));
+    }
+
+    /**
+     * 提交练习(顺序练习和随机练习)
+     * @param userTestDTO
+     * @return
+     */
+    @RequestMapping("/submitTest")
+    public ResponseEntity submitTest(@RequestBody UserTestDTO userTestDTO){
+        List<Boolean> list = userTestService.submitTest(userTestDTO,getUserId());
+        return ResponseEntity.ok(SuccessTip.create(list,"请求成功"));
+    }
+
+    /**
+     * 提交练习(模拟练习)
+     * @param testId
+     * @param answers
+     * @return
+     */
+    @RequestMapping("/submitMock")
+    public ResponseEntity submitMock(int testId,String answers){
+        List<Boolean> list = userTestService.submitMock(testId,answers,getUserId());
         return ResponseEntity.ok(SuccessTip.create(list,"请求成功"));
     }
 }
